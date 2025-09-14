ed3c9f2af16789e6ebc2eab6ccd04be323b995fb
Robin <lxs5171@mavs.uta.edu>
2025-09-14 00:37:47 -0500

    to use private docker registry on K8s with OpenWhisk

--- sebs/openwhisk/openwhisk.py
+++ sebs/openwhisk/openwhisk.py
@@ -206,6 +206,7 @@ class OpenWhisk(System):
                         code_package.language_name,
                         code_package.language_version,
                         code_package.architecture,
+                        registry=self.config.resources.docker_registry,
                     )
                     subprocess.run(
                         [
